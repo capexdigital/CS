0369c0830d720cde1eec9354cce1561fcfd28264
Joao Ferreira <you@example.com>
2025-04-12 02:53:35 -0300

    Last update

--- Algoritimos/Exerc_2.py
+++ Algoritimos/Exerc_2.py
@@ -1,31 +1,47 @@
 # GELADOS JPF
+print('Bem-vindo a Loja de Gelados do João Paulo Ferreira')
+print('AC - Açai')
+print('CP - Cupuaçu')
 
-print('--- Bem-vindo a Loja de Gelados do João Paulo Ferreira ---')
-print('1 - Açai')
-print('2 - Cupuaçu')
-
+# Loop restringindo opções de escolha do usuário
 while True:
-    sabor = int(input('Selecione o sabor desejado:')) 
-    tamanho = int(input('Selecione o tamanho desejado:'))
-    print('P')
-    print('M')
-    print('G')
-
-    if (sabor == 1 ):
-        print('Açai')
-    elif (sabor == 2 ):
-        print('Cupuaçu')
+    sabor = str(input('Selecione o sabor desejado:')) 
+    if sabor in ['AC', 'ac']:
+        print('P - R$ 11')
+        print('M - R$ 16')
+        print('G - R$ 20')
+    
+    elif sabor in ['CP', 'cp']:
+        print('P - R$ 9')
+        print('M - R$ 14')
+        print('G - R$ 18')
         break
     else:
-        print('Sabor Inválido. Digite Novamente')
+        print('Sabor Inválido. Tente novamente')
         
-# input para escolha do tamanho P M G 
-
-
 # implementar if/else com cada uma das combinações de sabor e tamanho
-
+while True:
+    tamanho = str(input('Selecione o tamanho desejado:'))
+    if tamanho in ['p', 'P'] and sabor in ['AC', 'ac']:
+        print('AÇAI PEQUENO')
+    elif tamanho in ['p', 'P'] and sabor in ['CP', 'cp']:
+        print('CUPUAÇU PEQUENO')
+    elif tamanho in ['m', 'M'] and sabor in ['AC', 'ac']:
+        print('AÇAI MÉDIO')
+    elif tamanho in ['m', 'M'] and sabor in ['CP', 'cp']:
+        print('CUPUAÇU MÉDIO')
+    elif tamanho in ['g', 'G'] and sabor in ['AC', 'ac']:
+        print('AÇAI GRANDE')
+    elif tamanho in ['g', 'G'] and sabor in ['CP', 'cp']:
+        print('CUPUAÇU GRANDE')        
+        break
+    else:
+        print('Tamanho inválido')
 
 # acumulador para somar valores dos pedidos
-
+while True:
+    qtd = int(input('Selecione a quantidade desejada:'))
+    print(qtd)
+    break
 
 
--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 This project is designed to help me store and track my studies in computer science.
 
-It allows me to upload and manage study materials, organize them by category, and search through them as my collection grows. This repository serves as both a personal study tracker and a reference for best practices
+It allows me to upload and manage study materials, organize them by category, and search through them as my collection grows. This repository serves as both a personal study tracker and a reference for best practices.
