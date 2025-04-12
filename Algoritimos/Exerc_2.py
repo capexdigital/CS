fc27d9220e4071d1cbff590fa4bd022d6d16a170
Joao Ferreira <you@example.com>
2025-04-11 23:16:41 -0300

    Updated

---
+++ Algoritimos/Exerc_2.py
@@ -0,0 +1,31 @@
+# GELADOS JPF
+
+print('--- Bem-vindo a Loja de Gelados do João Paulo Ferreira ---')
+print('1 - Açai')
+print('2 - Cupuaçu')
+
+while True:
+    sabor = int(input('Selecione o sabor desejado:')) 
+    tamanho = int(input('Selecione o tamanho desejado:'))
+    print('P')
+    print('M')
+    print('G')
+
+    if (sabor == 1 ):
+        print('Açai')
+    elif (sabor == 2 ):
+        print('Cupuaçu')
+        break
+    else:
+        print('Sabor Inválido. Digite Novamente')
+        
+# input para escolha do tamanho P M G 
+
+
+# implementar if/else com cada uma das combinações de sabor e tamanho
+
+
+# acumulador para somar valores dos pedidos
+
+
+
