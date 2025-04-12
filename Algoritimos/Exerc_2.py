4e5649bbcd8356036613110e229f95c8f7c2011c
Joao Ferreira <you@example.com>
2025-04-12 11:51:31 -0300

    Last update

--- Algoritimos/Exerc_2.py
+++ Algoritimos/Exerc_2.py
@@ -1,31 +1,35 @@
 # GELADOS JPF
-print('Bem-vindo a Loja de Gelados do João Paulo Ferreira')
+print('Bem-vindo a Loja de Gelados do João Paulo Ferreira\n')
+print('--- MENU ---')
 print('AC - Açai')
-print('CP - Cupuaçu')
+print('CP - Cupuaçu\n')
+
+total = 0
 
 # Loop restringindo opções de escolha do usuário
 while True:
-    sabor = str(input('Selecione o sabor desejado:')) 
-    if sabor in ['AC', 'ac']:
-        print('P - R$ 11')
+    sabor = input("Selecione o sabor desejado:")
+    if (sabor == 'AC' or sabor == 'ac'):
+        print('\nP - R$ 11')
         print('M - R$ 16')
-        print('G - R$ 20')
+        print('G - R$ 20\n')
+        break
     
-    elif sabor in ['CP', 'cp']:
-        print('P - R$ 9')
+    elif (sabor == 'CP' or sabor == 'cp'):
+        print('\nP - R$ 9')
         print('M - R$ 14')
-        print('G - R$ 18')
+        print('G - R$ 18\n')
         break
     else:
-        print('Sabor Inválido. Tente novamente')
+        print('\nSabor Inválido. Tente novamente\n')
         
 # implementar if/else com cada uma das combinações de sabor e tamanho
 while True:
-    tamanho = str(input('Selecione o tamanho desejado:'))
+    tamanho = input("Selecione o tamanho desejado:")
     if tamanho in ['p', 'P'] and sabor in ['AC', 'ac']:
-        print('AÇAI PEQUENO')
+        print('\nAÇAI PEQUENO\n')
     elif tamanho in ['p', 'P'] and sabor in ['CP', 'cp']:
-        print('CUPUAÇU PEQUENO')
+        print('\nCUPUAÇU PEQUENO\n')
     elif tamanho in ['m', 'M'] and sabor in ['AC', 'ac']:
         print('AÇAI MÉDIO')
     elif tamanho in ['m', 'M'] and sabor in ['CP', 'cp']:
@@ -34,14 +38,16 @@ while True:
         print('AÇAI GRANDE')
     elif tamanho in ['g', 'G'] and sabor in ['CP', 'cp']:
         print('CUPUAÇU GRANDE')        
-        break
-    else:
-        print('Tamanho inválido')
+    break
+else:
+    print('Tamanho inválido')
 
 # acumulador para somar valores dos pedidos
 while True:
     qtd = int(input('Selecione a quantidade desejada:'))
-    print(qtd)
+    if tamanho in ['p', 'P'] and sabor in ['AC', 'ac']:
+        total = total + qtd * 11
+        print(f'Total R$ {total}')
     break
 
 
